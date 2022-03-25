cc05a6d7131bda8c54f727c2da44d7cfab68095b
Yashpreet Kaur Virk <69677307+DefaultSpieler@users.noreply.github.com>
2022-03-25 15:28:22 +0530

    Add files via upload

---
+++ 100-days-of-python/Day1/1.4_variables.py
@@ -0,0 +1,9 @@
+a = input("Enter first Value")
+b = input("Enter Second Value")
+
+c = a
+b = a
+c = b
+
+print("a = " + a)
+print("b = " + b)
---
+++ 100-days-of-python/Day1/1.5_band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of the city you grew up in ?/n")
+petName = input("What is the name of your pet?/n")
+
+print(city + petName)
